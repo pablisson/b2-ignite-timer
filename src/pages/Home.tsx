a77e319a2fc76b31cc3834aa82f3411130e409c8
Páblisson Araújo <pablissonaraujo@gmail.com>
2025-03-20 15:54:49 -0300

    criação do formulario

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,27 @@
-export function Home(){
+export function Home() {
   return (
-    <h1>Home</h1>
-  )
+    <div>
+      <form action="">
+        <div>
+          <label htmlFor="">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="">durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">Iniciar</button>
+      </form>
+    </div>
+  );
 }
