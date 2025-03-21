e51b0d0288d4deca3127b709c51e8308b39312a5
Páblisson Araújo <pablissonaraujo@gmail.com>
2025-03-21 09:30:44 -0300

    ajustes e melhorias no dados projeto

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -90,9 +90,9 @@ const BaseInput = styled.input`
   color: ${(props) => props.theme["gray-100"]};
 
   &:focus {
-  box-shadow: none;
-  border-color: ${(props) => props.theme["green-500"]};
-};
+    box-shadow: none;
+    border-color: ${(props) => props.theme["green-500"]};
+  }
 
   &::placeholder {
     color: ${(props) => props.theme["gray-500"]};
@@ -101,6 +101,10 @@ const BaseInput = styled.input`
 
 export const TaskInput = styled(BaseInput)`
   flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
 `;
 
 export const MinutesAmountInput = styled(BaseInput)`
